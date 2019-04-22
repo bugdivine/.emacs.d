fddaf006f4cf932e2f8370624117a56e1ce525a7
Shubham Jain <bugdivine@gmail.com>
2019-04-23 02:30:44 +0530

    Added emacs customizations

---
+++ init.el
@@ -0,0 +1,8 @@
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+
+(require 'init-elpa)
+(require 'init-ui)
+(require 'init-editing)
+; (require 'init-navigation)
+
+(provide 'init)
