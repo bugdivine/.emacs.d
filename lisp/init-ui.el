72d27d73d73e8ac8ed9fa64a3c8b6e497cbcd697
Shubham Jain <bugdivine@gmail.com>
2019-07-07 22:53:04 +0530

    Increased font size to 150

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -11,7 +11,7 @@
 (when (fboundp 'scroll-bar-mode)
   (scroll-bar-mode -1))
 
-(set-face-attribute 'default nil :family "Monaco" :height 120)
+(set-face-attribute 'default nil :family "Monaco" :height 150)
 (setq-default line-spacing 0.4)
 
 (setq
